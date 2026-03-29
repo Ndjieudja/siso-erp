3c1c6ca3f48c9e5f6d3559f7839eb95d7f03f19d
Gabriel <gabrielndjieudja@gmail.com>
2026-03-29 09:50:01 +0100

    ebauche custom login

---
+++ Custom_addons/siso_custom_login/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    HuRIS WEB based on Odoo, Open Source Management Solution
+#    Copyright (C) 2023 (<http://cloud-tech.cm>).
+#    Martial MBE < cloudtech.cm@gmail.com | def.martial@gmail.com >
+#
+##############################################################################
+
+from . import models
+
