fc53e92367c744b38de5a91deca366de655d8cce
Gabriel <gabrielndjieudja@gmail.com>
2026-03-30 19:44:19 +0100

    custom hader in according to entreprise

--- Custom_addons/siso_porfolio/__manifest__.py
+++ Custom_addons/siso_porfolio/__manifest__.py
@@ -1,3 +1,14 @@
+# -*- coding: utf-8 -*-
+####################################################################################
+#
+#    SISO WEB based on Odoo, Open Source Management Solution
+#    Copyright (C) 2023 (<https://ssolutionets.com).
+#    Martial MBE < gabrielndjieudja@gmail.com | gabrielndjieudja@ssolutionets.com >
+#    See LICENSE file for full copyright and licensing details.
+#
+####################################################################################
+
+
 {
     'name': "SISO Portfolio",
     'version': '1.0',
--- Custom_addons/siso_porfolio/controllers/main.py
+++ Custom_addons/siso_porfolio/controllers/main.py
@@ -1,4 +1,15 @@
-# --*-- encoding:utf8 --*--
+# -*- coding: utf-8 -*-
+####################################################################################
+#
+#    SISO WEB based on Odoo, Open Source Management Solution
+#    Copyright (C) 2023 (<https://ssolutionets.com).
+#    Martial MBE < gabrielndjieudja@gmail.com | gabrielndjieudja@ssolutionets.com >
+#    See LICENSE file for full copyright and licensing details.
+#
+####################################################################################
+
+
+
 
 from odoo import http
 from odoo.http import request
